cc3fb0929907df800510543121a0d1b84e24cdb6
SalemAbsolem <128633228+SalemAbsolem@users.noreply.github.com>
2024-08-05 15:12:01 +0000

    Deploying to gh-pages from @ SalemAbsolem/aquapro@ab9c6c6563e9e3adbdd5532cd54cb89e67708526 🚀

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,1 +1,1 @@
-var o=window.matchMedia("(max-width: 767.98px)"),t=window.matchMedia("(hover: hover)");export{t as HOVER,o as MOBILE_ONLY};
+var o=window.matchMedia("(max-width: 767.98px)"),t=window.matchMedia("(max-width: 1199.98px)"),a=window.matchMedia("(hover: hover)");export{a as HOVER,o as MOBILE_ONLY,t as NOT_DESKTOP};
